c908c8a5b37d720f958e490c810fd6f0387de0a3
GrandSteph <giloppe@gmail.com>
2020-05-06 11:35:20 +0200

    With ImageResize

--- SimpleScorerv01/Views/CircleImagePickerView.swift
+++ SimpleScorerv01/Views/CircleImagePickerView.swift
@@ -126,7 +126,7 @@ struct ImageResizerView: View {
                     HStack {
                         Spacer()
                         Button(action: {
-                            self.image = self.rectToCapture.uiImage!
+                            self.image = resizeImage(image: self.rectToCapture.uiImage!, targetSize: CGSize(width: 200, height: 200))
                             self.isPresented = false
                         }) {
                             Image(systemName: "checkmark")
@@ -298,4 +298,30 @@ extension UIImage {
         context.draw(cgImage, in: rect)
         return context.makeImage().map { UIImage(cgImage: $0) } ?? self
     }
+}
+
+func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+    let size = image.size
+
+    let widthRatio  = targetSize.width  / size.width
+    let heightRatio = targetSize.height / size.height
+
+    // Figure out what our orientation is, and use that to form the rectangle
+    var newSize: CGSize
+    if(widthRatio > heightRatio) {
+        newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+    } else {
+        newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+    }
+
+    // This is the rect that we've calculated out and this is what is actually used below
+    let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+    // Actually do the resizing to the rect using the ImageContext stuff
+    UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+    image.draw(in: rect)
+    let newImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+
+    return newImage!
 }
