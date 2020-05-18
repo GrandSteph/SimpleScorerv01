2a956b1e7a8f15ef1eb3f76d3ec78b55d1ab3861
Steph <giloppe@yahoo.com>
2020-05-18 16:39:00 +0200

    added pick from library functionality - No editing

--- SimpleScorerv01/Views/AvatarView.swift
+++ SimpleScorerv01/Views/AvatarView.swift
@@ -13,19 +13,32 @@ struct AvatarView: View {
     var name : String?
     var image : UIImage?
     
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var pickedImage: UIImage?
     
     @ViewBuilder
     var body: some View {
         
         ZStack {
-            if name != nil && name != "Name ?" {
+            
+            if pickedImage?.imageAsset != nil {
+                Image(uiImage: pickedImage!).renderingMode(.original)
+                .resizable()
+                .scaledToFill()
+                .clipShape(Circle())
+            } else if image?.imageAsset != nil {
+                Image(uiImage: image!).renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+            } else if name != nil && name != "Name ?" {
                 Text(name!.uppercased().prefix(1))
                     .fontWeight(.regular)
                     .font(.system(.largeTitle, design: .rounded))
                     .foregroundColor(Color.white)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .padding()
-                    .overlay(Circle().strokeBorder(Color.white, lineWidth: 2))
             } else {
                 Image(systemName: "camera")
                     .font(.system(size: 30, weight: .light, design: .default))
@@ -33,17 +46,19 @@ struct AvatarView: View {
                     .padding()
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .contentShape(Circle())
-                    .overlay(Circle().strokeBorder(Color.white, lineWidth: 2))
-            }
-            
-            if image?.imageAsset != nil {
-                Image(uiImage: image!).renderingMode(.original)
-                .resizable()
-                .scaledToFit()
-                .clipShape(Circle())
-                .overlay(Circle().strokeBorder(Color.offWhite, lineWidth: 1))
             }
         }
+        .overlay(Circle().strokeBorder(Color.white, lineWidth: 2))
+        .onTapGesture {
+                self.showingImagePicker = true
+        }.sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        pickedImage = resizeImage(image: inputImage, targetSize: CGSize(width: 200, height: 200))
     }
 }
 
@@ -67,5 +82,42 @@ struct CircleImage_Previews: PreviewProvider {
         }
         
         
+    }
+}
+
+// Temp while imagepicker is not great
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+
+    }
+    
+   func makeCoordinator() -> Coordinator {
+          Coordinator(self)
+      }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
     }
 }
